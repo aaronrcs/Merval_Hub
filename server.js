5e78428ab7c48729304a0326f81f3f9847951a03
aaronrcs <aromer61@calstatela.edu>
2019-10-23 03:14:43 -0700

    Updated server.js file

--- server.js
+++ server.js
@@ -6,31 +6,13 @@ const
 const
     app = express(),
     app2 = express(),
-    // server = require('https').Server(app2)
-    server = app2.listen(8081)
-    io = require('socket.io').listen(server);
+    server = require('http').Server(app2)
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8082")
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
-
-
-    const prevQueries = []
-
-    io.on('connection', socket => {
-
-        // Syncing the queries list in the root
-        socket.emit('refreshQueries', prevQueries)
-
-        // Adding the new queries to the list
-        socket.on('addQuery', data => {
-            prevQueries.push(data)
-            io.emit('successfulQuery', data)
-        })
-    })
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '.', '/front-end')))
@@ -45,11 +27,13 @@ if(process.env.NODE_ENV === 'production'){
     // redirecting to index.html to display our project
     app.get(/.*/,(req,res) => res.sendFile(__dirname + '/dist/index.html'))
 }
+// For testing locally
 // app.listen(8080, () => {
 //     console.log('Server is running')
 // })
+// For production
 app.listen(process.env.PORT);
 
 app2.use(express.static(path.join(__dirname, '.', '/front-end')))
-// require('./sockets')(server)
-// server.listen(8082)
+require('./sockets')(server)
+server.listen(8082)
