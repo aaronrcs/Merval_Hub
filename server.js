00bd61cbd00b96f8f3a1e0cdb640f8ad18338423
aaronrcs <aromer61@calstatela.edu>
2019-10-23 03:36:15 -0700

    Updated server.js file

--- server.js
+++ server.js
@@ -1,46 +1,18 @@
 const
     bodyParser = require('body-parser'),
-    // express = require('express'),
+    express = require('express'),
     path = require('path')
 
-// const
-//     app = express(),
-//     app2 = express(),
-//     server = require('http').Server(app2)
-//     io = require('socket.io')(server)
-
-const express = require('express'),
-    http = require('http');
-    app = express();
-    app2 = express();
-    server = http.createServer(app2);
-    io = require('socket.io').listen(server);
-
-server.listen(8082);
-
-
+const
+    app = express(),
+    app2 = express(),
+    server = require('http').Server(app2)
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
-
-    io.connect('http://localhost:8082');
-
-    const prevQueries = []
-
-    io.on('connection', socket => {
-
-        // Syncing the queries list in the root
-        socket.emit('refreshQueries', prevQueries)
-
-        // Adding the new queries to the list
-        socket.on('addQuery', data => {
-            prevQueries.push(data)
-            io.emit('successfulQuery', data)
-        })
-    })
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '.', '/front-end')))
@@ -55,7 +27,7 @@ if(process.env.NODE_ENV === 'production'){
     // redirecting to index.html to display our project
     app.get(/.*/,(req,res) => res.sendFile(__dirname + '/dist/index.html'))
 }
-// For testing locally
+// For tesing locally
 // app.listen(8080, () => {
 //     console.log('Server is running')
 // })
@@ -63,5 +35,5 @@ if(process.env.NODE_ENV === 'production'){
 app.listen(process.env.PORT);
 
 app2.use(express.static(path.join(__dirname, '.', '/front-end')))
-// require('./sockets')(server)
-// server.listen(8082)
+require('./sockets')(server)
+server.listen(8082)
