d6835620004102b4ecf105ddfa0cd48b2502985a
aaronrcs <aromer61@calstatela.edu>
2019-10-23 03:26:34 -0700

    Updated server.js file

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
-const
-    bodyParser = require('body-parser'),
-    express = require('express'),
-    path = require('path')
+// const
+//     bodyParser = require('body-parser'),
+//     express = require('express'),
+//     path = require('path')
 
-const
-    app = express(),
-    app2 = express(),
-    server = require('http').Server(app2)
+// const
+//     app = express(),
+//     app2 = express(),
+//     server = require('http').Server(app2)
+//     io = require('socket.io')(server)
+
+var express = require('express'),
+    http = require('http');
+var app = express();
+var app2 = express();
+var server = http.createServer(app2);
+var io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
+
+    io.connect('http://localhost:8082');
+
+    const prevQueries = []
+
+    io.on('connection', socket => {
+
+        // Syncing the queries list in the root
+        socket.emit('refreshQueries', prevQueries)
+
+        // Adding the new queries to the list
+        socket.on('addQuery', data => {
+            prevQueries.push(data)
+            io.emit('successfulQuery', data)
+        })
+    })
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '.', '/front-end')))
