8f152a7fa7fd1e36e25bd01850d10a55c6b852f8
aaronrcs <aromer61@calstatela.edu>
2019-10-30 03:48:19 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ app.use(express.static(path.join(__dirname, '.', '/front-end')))
 app.use(require('./api/routes')())
 
 // Code for production
-// if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     // Set static path
     app.use(express.static(__dirname + '/dist/'))
 
     // redirecting to index.html to display our project
     app.get(/.*/,(req,res) => res.sendFile(__dirname + '/dist/index.html'))
-// }
+}
 // For tesing locally
 app.listen(process.env.PORT || 8080, () => {
     console.log('Server is running');
